4ad3665804aee8e34301036d212dd31d0f4a9374
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-10 23:33:23 -0300

    i add price-list component to project

---
+++ src/app/public/price-list/price-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-price-list',
+  imports: [],
+  templateUrl: './price-list.component.html',
+  styleUrl: './price-list.component.scss'
+})
+export class PriceListComponent {
+
+}
