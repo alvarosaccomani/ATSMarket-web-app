e0d64fb97b6c09b6f2143a78ff31bb3039c49ef7
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-10 23:52:32 -0300

    i add price-list route to public.routes.ts file

--- src/app/public/public.routes.ts
+++ src/app/public/public.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 
 import { PublicLayoutComponent } from './public-layout/public-layout.component';
 import { HomeComponent } from './home/home.component';
+import { PriceListComponent } from './price-list/price-list.component';
 
 export const PUBLIC_ROUTES: Routes = [
     {
         path: '',
         component: PublicLayoutComponent,
         children: [
-            { path: 'home', component: HomeComponent }
+            { path: 'home', component: HomeComponent },
+            { path: 'price-list', component: PriceListComponent },
         ]
     }
 ];
