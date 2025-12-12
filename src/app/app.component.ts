ede8a2b272b61ed1e905c39bfb1f8990c031157d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-12 20:48:29 -0300

    i add nav-bar component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NavBarComponent } from '@components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true, // Si usas Angular 15+, usa standalone
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NavBarComponent,
+    NzLayoutModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
