58a5252f7409ac9d8878f27477149e05d3f45c17
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-12 20:47:34 -0300

    i add ** path to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
     {
         path: 'public',
         loadChildren: () => import('./public/public.routes').then(m => m.PUBLIC_ROUTES),
-    }
+    },
+    { path: '**', redirectTo: 'public/home' }
 ];
