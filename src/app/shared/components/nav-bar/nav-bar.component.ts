30ebcf659c81665acae1053b76610fcb6a48c69c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-13 19:45:45 -0300

    inject cartService in nav-bar component

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { CartItemInterface } from '@interfaces/cart-item.interface';
+import { CartService } from '@services/cart.service';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
+    AsyncPipe,
+    RouterLink,
     FormsModule,
     NzDrawerModule,
     NzLayoutModule,
@@ -24,12 +31,15 @@ export class NavBarComponent {
   // Propiedad para mostrar el número de ítems en el carrito
   // Esto debe ser alimentado por tu CartService real.
   public cartItemCount: number = 0;
+  public cartItems$: Observable<CartItemInterface[]>;
 
   // Si usas ngModel en el input, necesitas importar FormsModule y ReactiveFormsModule
   // en tu app.module.ts.
 
-  constructor() {
-    // Aquí puedes inicializar cartItemCount con datos del servicio.
+  constructor(
+    private cartService: CartService
+  ) {
+    this.cartItems$ = this.cartService.cartItems$;
   }
 
   // Maneja el cambio en el campo de búsqueda.
