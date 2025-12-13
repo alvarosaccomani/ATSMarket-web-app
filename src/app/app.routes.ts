3f6d4085968746d9245068dbf5085e46cf5c0ab0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-12 21:33:30 -0300

    i add user routes to app.routes.ts file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
         path: 'public',
         loadChildren: () => import('./public/public.routes').then(m => m.PUBLIC_ROUTES),
     },
+    {
+        path: 'user',
+        loadChildren: () => import('./user/user.routes').then(m => m.USER_ROUTES),
+    },
     { path: '**', redirectTo: 'public/home' }
 ];
