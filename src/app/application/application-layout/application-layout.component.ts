267c77eb7ae6510ae60b0b9424c24d24ffb58d5d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-13 21:32:36 -0300

    add router-outlet to application layout component

--- src/app/application/application-layout/application-layout.component.ts
+++ src/app/application/application-layout/application-layout.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-application-layout',
-  imports: [],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './application-layout.component.html',
   styleUrl: './application-layout.component.scss'
 })
