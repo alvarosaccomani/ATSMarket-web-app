0830db6d945a41509494b2d4eddc48bc1e80b783
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2025-12-14 09:19:59 -0300

    i add NzLayoutModule to application-layout component

--- src/app/application/application-layout/application-layout.component.ts
+++ src/app/application/application-layout/application-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 
 @Component({
   selector: 'app-application-layout',
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    NzLayoutModule
   ],
   templateUrl: './application-layout.component.html',
   styleUrl: './application-layout.component.scss'
