6c9b405bfbd5a3d3e1721eaf86e048933a04cba3
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-07 19:16:47 -0300

    i add market-home component to project

---
+++ src/app/public/market-home/market-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-market-home',
+  imports: [],
+  templateUrl: './market-home.component.html',
+  styleUrl: './market-home.component.scss'
+})
+export class MarketHomeComponent {
+
+}
