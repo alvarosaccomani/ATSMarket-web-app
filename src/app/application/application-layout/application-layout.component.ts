908f73298ce53a40922b7dc9b16787f34184fa0d
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-08 18:43:14 -0300

    i add app-side-bar and app-application-bar to application-layout component

--- src/app/application/application-layout/application-layout.component.ts
+++ src/app/application/application-layout/application-layout.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { SideBarComponent } from '@components/side-bar/side-bar.component';
+import { ApplicationBarComponent } from '@components/application-bar/application-bar.component';
 
 @Component({
   selector: 'app-application-layout',
   imports: [
     RouterOutlet,
-    NzLayoutModule
+    NzLayoutModule,
+    SideBarComponent,
+    ApplicationBarComponent
   ],
   templateUrl: './application-layout.component.html',
   styleUrl: './application-layout.component.scss'
