9101ff8acd4eb86223389b4b09a5e250c9c58a35
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-08 18:42:05 -0300

    i add code to application-bar component

--- src/app/shared/components/application-bar/application-bar.component.ts
+++ src/app/shared/components/application-bar/application-bar.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-application-bar',
-  imports: [],
+  imports: [
+    NzLayoutModule,
+    NzAvatarModule,
+    NzIconModule
+  ],
   templateUrl: './application-bar.component.html',
   styleUrl: './application-bar.component.scss'
 })
