ac29e8e31c692c02ac9d44f334f986c2412b44f6
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-08 18:40:21 -0300

    i add isCollapsed property to side-bar component

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-side-bar',
-  imports: [],
+  imports: [
+    CommonModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzIconModule,
+    RouterLink
+  ],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent {
+
+  public isCollapsed: boolean = false;
 
 }
