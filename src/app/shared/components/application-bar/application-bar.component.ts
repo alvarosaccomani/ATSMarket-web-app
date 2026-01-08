413af3967c7c04319fa09682c318d04e598bc0c7
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-08 16:07:55 -0300

    i add application-bar component to project

---
+++ src/app/shared/components/application-bar/application-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-application-bar',
+  imports: [],
+  templateUrl: './application-bar.component.html',
+  styleUrl: './application-bar.component.scss'
+})
+export class ApplicationBarComponent {
+
+}
