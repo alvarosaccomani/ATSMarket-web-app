8a0bd1ad9ce52e3e884920bd09fe7afdf8005132
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-09 17:12:03 -0300

    i add the files environment.ts and environment.prod.ts to project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.atsmarket.com.ar/api/',
+  appName: 'ATS Market'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:3002/api/',
+  appName: 'ATS Market (Dev)'
+};
