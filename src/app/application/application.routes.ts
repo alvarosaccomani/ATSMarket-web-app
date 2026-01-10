c9854ab16256e22ef56378f885ace280f8c2edd4
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-01-10 20:50:47 -0300

    i add product/:pro_uuid route to application.routes.ts

--- src/app/application/application.routes.ts
+++ src/app/application/application.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
 import { PriceListComponent } from './price-list/price-list.component';
 import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './product/product.component';
 
 export const APPLICATION_ROUTES: Routes = [
     {
@@ -11,6 +12,7 @@ export const APPLICATION_ROUTES: Routes = [
         children: [
             { path: 'price-list', component: PriceListComponent },
             { path: 'products', component: ProductsComponent },
+            { path: 'product/:pro_uuid', component: ProductComponent }
         ]
     }
 ];
