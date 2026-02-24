0d052bce20831ce8e0f23b69ccb5cddfd81711d3
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-24 14:57:09 -0300

    i add dist-price-list route to application.routes.ts

--- src/app/application/application.routes.ts
+++ src/app/application/application.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
 import { PriceListComponent } from './price-list/price-list.component';
+import { DistPriceListComponent } from './dist-price-list/dist-price-list.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './product/product.component';
 import { ProductVariationComponent } from './product-variation/product-variation.component';
@@ -12,6 +13,7 @@ export const APPLICATION_ROUTES: Routes = [
         component: ApplicationLayoutComponent,
         children: [
             { path: 'price-list', component: PriceListComponent },
+            { path: 'dist-price-list', component: DistPriceListComponent },
             { path: 'products', component: ProductsComponent },
             { path: 'product/:pro_uuid', component: ProductComponent },
             { path: 'product-variation/:pro_uuid/:prov_uuid', component: ProductVariationComponent }
