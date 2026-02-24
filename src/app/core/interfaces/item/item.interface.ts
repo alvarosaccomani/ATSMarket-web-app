79ee2052612f6e88bc3bfe30708f5c51b8f07099
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-02-24 14:44:49 -0300

    add item, item-results and index interfaces to project

---
+++ src/app/core/interfaces/item/item.interface.ts
@@ -0,0 +1,8 @@
+export interface ItemInterface {
+    cmp_uuid: string;
+    itm_uuid: string;
+    itm_name: string;
+    itm_description: string;
+    itm_createdat: Date;
+    itm_updatedat: Date;
+}
