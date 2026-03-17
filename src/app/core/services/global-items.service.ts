b3750f117d06ee6e45a3b16a4bef55b6276cf957
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-17 18:51:55 -0300

    i add method getGlobalItems to global-items service

--- src/app/core/services/global-items.service.ts
+++ src/app/core/services/global-items.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { GlobalItemResults } from '@interfaces/global-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalItemsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todos los rubros globales.
+   * En una aplicación real, aquí usarías this.http.get<IItem[]>('URL_DE_TU_API').
+   * @returns Observable de un array de rubros globales.
+   */
+  public getGlobalItems(): Observable<GlobalItemResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+
+    let params = new HttpParams();
+
+    return this._http.get<GlobalItemResults>(`${environment.apiUrl}global-items`, { headers, params });
+  }
 }
