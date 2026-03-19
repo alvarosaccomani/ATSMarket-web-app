ac54a79be510d514992af1e6e76788ca7643f237
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-19 11:04:27 -0300

    i add method login to users service

--- src/app/core/services/users.service.ts
+++ src/app/core/services/users.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  public login(user: any, gettoken: string | null = null): Observable<any> {
+    if (gettoken != null) {
+      user.gettoken = gettoken;
+    }
+
+    let params = JSON.stringify(user);
+    let headers = new HttpHeaders().set('content-type', 'application/json');
+
+    return this._http.post(environment.apiUrl + 'login', params, { headers: headers });
+  }
 }
