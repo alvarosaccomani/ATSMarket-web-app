876388dc96da49c111ac38a977d87d461e80f777
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-19 08:50:16 -0300

    i add method getGlobalCategories to global-categories service

--- src/app/core/services/global-categories.service.ts
+++ src/app/core/services/global-categories.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { GlobalCategoryResults } from '@interfaces/global-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalCategoriesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todos las categorias globales.
+   * En una aplicación real, aquí usarías this.http.get<IItem[]>('URL_DE_TU_API').
+   * @returns Observable de un array de categorias globales.
+   */
+  public getGlobalCategories(): Observable<GlobalCategoryResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+
+    let params = new HttpParams();
+
+    return this._http.get<GlobalCategoryResults>(`${environment.apiUrl}global-categories`, { headers, params });
+  }
 }
