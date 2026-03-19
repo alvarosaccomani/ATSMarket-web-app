6a5324e43ccc0ec106ef8c333c35d9cb211821b2
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-19 09:31:51 -0300

    add user-rol-company, user-rol-company-results and index interfaces to project

---
+++ src/app/core/interfaces/user-rol-company/user-rol-company.interface.ts
@@ -0,0 +1,14 @@
+import { CompanyInterface } from "../company"
+import { RolInterface } from "../rol"
+import { UserInterface } from "../user/user.interface"
+
+export interface UserRolCompanyInterface {
+    cmp_uuid: string | null,
+    cmp: CompanyInterface | null,
+    usr_uuid: string | null,
+    usr: UserInterface | null,
+    rol_uuid: string | null,
+    rol: RolInterface | null,
+    usrrolcmp_createdat: Date | null,
+    usrrolcmp_updatedat: Date | null
+}
