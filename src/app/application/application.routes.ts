54adb4e7f8f91ba3d76ee49800da9318766fa741
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-20 17:00:46 -0300

    i add company-settings/:cmp_uuid route to application.routes.ts

--- src/app/application/application.routes.ts
+++ src/app/application/application.routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
 import { MyCompaniesComponent } from './my-companies/my-companies.component';
 import { CompanyComponent } from './company/company.component';
+import { CompanySettingsComponent } from './company-settings/company-settings.component';
 import { PriceListComponent } from './price-list/price-list.component';
 import { DistPriceListComponent } from './dist-price-list/dist-price-list.component';
 import { ProductsComponent } from './products/products.component';
@@ -16,6 +17,7 @@ export const APPLICATION_ROUTES: Routes = [
         children: [
             { path: 'my-companies', component: MyCompaniesComponent },
             { path: 'company/:cmp_uuid', component: CompanyComponent },
+            { path: 'company-settings/:cmp_uuid', component: CompanySettingsComponent },
             { path: 'price-list', component: PriceListComponent },
             { path: 'dist-price-list', component: DistPriceListComponent },
             { path: 'products', component: ProductsComponent },
