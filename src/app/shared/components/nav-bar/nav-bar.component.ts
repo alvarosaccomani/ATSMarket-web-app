ce2faa871d9ea6326537abe6f44c95cac9be61ad
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-20 11:53:46 -0300

    i add method logout to nav-bar component

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
+import { RouterLink, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { Observable } from 'rxjs';
+
+// NG-ZORRO Modules
 import { NzDrawerModule } from 'ng-zorro-antd/drawer';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzBadgeModule } from 'ng-zorro-antd/badge';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzAvatarModule } from 'ng-zorro-antd/avatar';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 import { CartItemInterface } from '@interfaces/cart-item.interface';
 import { CartService } from '@services/cart.service';
+import { SessionService } from '@services/session.service';
 
 @Component({
   selector: 'app-nav-bar',
   imports: [
+    CommonModule,
     AsyncPipe,
     RouterLink,
     FormsModule,
@@ -21,27 +29,33 @@ import { CartService } from '@services/cart.service';
     NzLayoutModule,
     NzIconModule,
     NzBadgeModule,
-    NzInputModule
+    NzInputModule,
+    NzAvatarModule,
+    NzDropDownModule,
+    NzButtonModule
   ],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
-  // Propiedad para mostrar el número de ítems en el carrito
-  // Esto debe ser alimentado por tu CartService real.
   public cartItemCount: number = 0;
   public cartItems$: Observable<CartItemInterface[]>;
-  // Propiedad para controlar la visibilidad del Drawer
   public drawerVisible: boolean = false;
 
-  // Si usas ngModel en el input, necesitas importar FormsModule y ReactiveFormsModule
-  // en tu app.module.ts.
+  // Usuario
+  public userIdentity: any = null;
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private _sessionService: SessionService,
+    private _router: Router
   ) {
     this.cartItems$ = this.cartService.cartItems$;
+  }
+
+  ngOnInit(): void {
+    this.userIdentity = this._sessionService.getIdentity();
   }
 
   // Maneja el cambio en el campo de búsqueda.
@@ -61,5 +75,11 @@ export class NavBarComponent {
 
   public closeDrawer(): void {
     this.drawerVisible = false;
+  }
+
+  public logout(): void {
+    this._sessionService.logout();
+    this.userIdentity = null;
+    this._router.navigate(['/auth/login']);
   }
 }
