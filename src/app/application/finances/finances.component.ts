442713be6ea2b89c7b07c41fafb04496453c499a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-25 08:55:26 -0300

    i add finances component to project

---
+++ src/app/application/finances/finances.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-finances',
+  imports: [],
+  templateUrl: './finances.component.html',
+  styleUrl: './finances.component.scss'
+})
+export class FinancesComponent {
+
+}
