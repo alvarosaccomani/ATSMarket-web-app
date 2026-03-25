08eda9e88c04a47f0110b3f881d0f712b172d603
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-25 10:24:20 -0300

    add address, address-results and index interfaces to project

---
+++ src/app/core/interfaces/address/address.interface.ts
@@ -0,0 +1,12 @@
+export interface AddressInterface {
+    cmp_uuid: string;
+    adr_uuid: string;
+    cus_uuid: string;
+    sup_uuid: string;
+    adr_address: string;
+    adr_city: string;
+    adr_province: string;
+    adr_postalcode: string;
+    adr_createdat: Date;
+    adr_updatedat: Date;
+}
