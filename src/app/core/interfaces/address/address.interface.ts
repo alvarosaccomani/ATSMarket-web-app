44a041dc24c3073eaa0270695b6312e2d0833daa
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-03-25 17:32:57 -0300

    i add adr_alias,   adr_recipientname, adr_contactphone, adr_reference, adr_country, adr_lat and adr_lng to address interface

--- src/app/core/interfaces/address/address.interface.ts
+++ src/app/core/interfaces/address/address.interface.ts
@@ -3,10 +3,17 @@ export interface AddressInterface {
     adr_uuid: string;
     cus_uuid: string;
     sup_uuid: string;
+    adr_alias: string;
+    adr_recipientname: string;
+    adr_contactphone: string;
+    adr_reference: string;
+    adr_country: string;
     adr_address: string;
     adr_city: string;
     adr_province: string;
     adr_postalcode: string;
+    adr_lat: number;
+    adr_lng: number;
     adr_createdat: Date;
     adr_updatedat: Date;
 }
