9ac9738ccf35731c5662de2cfefb47372ddb92c6
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-04-29 15:25:21 -0300

    i add materials route to application.routes.ts

--- src/app/application/application.routes.ts
+++ src/app/application/application.routes.ts
@@ -6,6 +6,7 @@ import { CompanyComponent } from './company/company.component';
 import { CompanySettingsComponent } from './company-settings/company-settings.component';
 import { PriceListComponent } from './price-list/price-list.component';
 import { DistPriceListComponent } from './dist-price-list/dist-price-list.component';
+import { MaterialsComponent } from './materials/materials.component';
 import { ProductsComponent } from './products/products.component';
 import { ProductComponent } from './product/product.component';
 import { ProductVariationComponent } from './product-variation/product-variation.component';
@@ -26,6 +27,7 @@ export const APPLICATION_ROUTES: Routes = [
             { path: 'company-settings/:cmp_uuid', component: CompanySettingsComponent },
             { path: 'price-list', component: PriceListComponent },
             { path: 'dist-price-list', component: DistPriceListComponent },
+            { path: 'materials', component: MaterialsComponent },
             { path: 'products', component: ProductsComponent },
             { path: 'product/:pro_uuid', component: ProductComponent },
             { path: 'product-variation/:pro_uuid/:prov_uuid', component: ProductVariationComponent },
