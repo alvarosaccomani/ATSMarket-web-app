5cdde501da613415604aa30ab2a868c8c78e838f
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-04-30 11:19:08 -0300

    i add method getMaterials to materials service

--- src/app/core/services/materials.service.ts
+++ src/app/core/services/materials.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { MaterialResults } from '@interfaces/material';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MaterialsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todos los materiales.
+   * @returns Observable de un array de materiales.
+   */
+  public getMaterials(cmp_uuid: string): Observable<MaterialResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+
+    let params = new HttpParams();
+
+    return this._http.get<MaterialResults>(`${environment.apiUrl}materials/${cmp_uuid}`, { headers, params });
+  }
 }
