b076a3f4a02cc334ca766c0b48a0a7a97a30d6b0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-04-30 13:25:05 -0300

    i add method getGlobalMaterials to global-materials service

--- src/app/core/services/global-materials.service.ts
+++ src/app/core/services/global-materials.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { GlobalMaterialResults } from '@interfaces/global-material';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GlobalMaterialsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todos los materiales globales.
+   * @returns Observable de un array de materiales globales.
+   */
+  public getGlobalMaterials(): Observable<GlobalMaterialResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+
+    let params = new HttpParams();
+
+    return this._http.get<GlobalMaterialResults>(`${environment.apiUrl}global-materials`, { headers, params });
+  }
 }
