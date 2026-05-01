1c846fd123622688f15df36afa4600a6b44eaccb
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-01 10:27:13 -0300

    i add property gitm_image to global-item interface

--- src/app/core/interfaces/global-item/global-item.interface.ts
+++ src/app/core/interfaces/global-item/global-item.interface.ts
@@ -2,6 +2,7 @@ export interface GlobalItemInterface {
     gitm_uuid: string;
     gitm_name: string;
     gitm_description: string;
+    gitm_image: string;
     gitm_createdat: Date;
     gitm_updatedat: Date;
 }
