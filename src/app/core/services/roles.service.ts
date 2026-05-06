7d557e9fe411fca9c7e8bcd5ed936cf7d6cc62de
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-06 09:20:43 -0300

    i add methods getRoles and getRolById to roles service

--- src/app/core/services/roles.service.ts
+++ src/app/core/services/roles.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RolesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todos los roles.
+   * @returns Observable de un array de roles.
+   */
+  public getRoles(filter: string = '', page: number = 1, perPage: number = 100): Observable<any> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this._http.get<any>(`${environment.apiUrl}roles/${filter}/${page}/${perPage}`, { headers });
+  }
+
+  /**
+   * Obtiene un rol por su UUID.
+   */
+  public getRolById(rol_uuid: string): Observable<{ data: any }> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this._http.get<{ data: any }>(`${environment.apiUrl}rol/${rol_uuid}`, { headers });
+  }
 }
