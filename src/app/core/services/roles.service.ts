b93b42b69ec3101203e3d64aa1dabe855a58d511
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-06 14:35:08 -0300

    i add params by HttpParams in method getRoles in roles service

--- src/app/core/services/roles.service.ts
+++ src/app/core/services/roles.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 
@@ -18,7 +18,22 @@ export class RolesService {
    */
   public getRoles(filter: string = '', page: number = 1, perPage: number = 100): Observable<any> {
     const headers = new HttpHeaders().set('content-type', 'application/json');
-    return this._http.get<any>(`${environment.apiUrl}roles/${filter}/${page}/${perPage}`, { headers });
+
+    let params = new HttpParams();
+
+    if (filter) {
+      params = params.set('filter', filter);
+    }
+
+    if (page) {
+      params = params.set('page', page.toString());
+    }
+
+    if (perPage) {
+      params = params.set('perPage', perPage.toString());
+    }
+
+    return this._http.get<any>(`${environment.apiUrl}roles`, { headers });
   }
 
   /**
