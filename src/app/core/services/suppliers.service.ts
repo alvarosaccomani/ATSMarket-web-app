60af94ecebfacc12698a9c89f9daba6f18440dfb
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-09 22:29:13 -0300

    i add method getSuppliers to suppliers service

--- src/app/core/services/suppliers.service.ts
+++ src/app/core/services/suppliers.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { SupplierResults } from '@interfaces/supplier';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SuppliersService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todos los proveedores.
+   * @returns Observable de un array de proveedores.
+   */
+  public getSuppliers(cmp_uuid: string): Observable<SupplierResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+
+    let params = new HttpParams();
+
+    return this._http.get<SupplierResults>(`${environment.apiUrl}suppliers/${cmp_uuid}`, { headers, params });
+  }
 }
