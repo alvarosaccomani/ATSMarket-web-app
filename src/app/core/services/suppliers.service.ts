f79556addee31a6a1470858903a90408ac691379
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-09 23:19:18 -0300

    i add methods getSupplierById, saveSupplier, and updateSupplier to suppliers service

--- src/app/core/services/suppliers.service.ts
+++ src/app/core/services/suppliers.service.ts
@@ -19,9 +19,24 @@ export class SuppliersService {
    */
   public getSuppliers(cmp_uuid: string): Observable<SupplierResults> {
     const headers = new HttpHeaders().set('content-type', 'application/json');
-
     let params = new HttpParams();
-
     return this._http.get<SupplierResults>(`${environment.apiUrl}suppliers/${cmp_uuid}`, { headers, params });
+  }
+
+  public getSupplierById(cmp_uuid: string, sup_uuid: string): Observable<any> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this._http.get(`${environment.apiUrl}supplier/${cmp_uuid}/${sup_uuid}`, { headers });
+  }
+
+  public saveSupplier(supplier: any): Observable<any> {
+    let params = JSON.stringify(supplier);
+    let headers = new HttpHeaders().set('content-type', 'application/json');
+    return this._http.post(`${environment.apiUrl}supplier`, params, { headers });
+  }
+
+  public updateSupplier(supplier: any): Observable<any> {
+    let params = JSON.stringify(supplier);
+    let headers = new HttpHeaders().set('content-type', 'application/json');
+    return this._http.put(`${environment.apiUrl}supplier/${supplier.cmp_uuid}/${supplier.sup_uuid}`, params, { headers });
   }
 }
