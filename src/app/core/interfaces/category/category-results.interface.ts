f3cd680a6d54b680ac4755be939b22f05f0f2821
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-09 22:28:38 -0300

    i rename properties category by item and categoryOf by itemOf in category-results interface

--- src/app/core/interfaces/category/category-results.interface.ts
+++ src/app/core/interfaces/category/category-results.interface.ts
@@ -1,8 +1,8 @@
 import { CategoryInterface } from "./category.interface";
 
 export interface CategoryResults {
-  category: number;
-  categoryOf: number;
+  item: number;
+  itemOf: number;
   numElements: number;
   totalPages: number;
   data: CategoryInterface[]
