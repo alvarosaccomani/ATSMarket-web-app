5e9fe510f97f6d6ad427a0f34c63fbc1d8af05f0
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-09 21:48:14 -0300

    i add method getCategories to categories service

--- src/app/core/services/categories.service.ts
+++ src/app/core/services/categories.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { CategoryResults } from '@interfaces/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todas las categorias.
+   * @returns Observable de un array de categorias.
+   */
+  public getCategories(cmp_uuid: string, itm_uuid: string): Observable<CategoryResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+
+    let params = new HttpParams();
+
+    return this._http.get<CategoryResults>(`${environment.apiUrl}categories/${cmp_uuid}/${itm_uuid}`, { headers, params });
+  }
 }
