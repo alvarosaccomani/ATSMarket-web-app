879074335b600c86c0320fb7b70198f743fc49a9
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-13 14:40:23 -0300

    i add method getCurrencies to currencies service

--- src/app/core/services/currencies.service.ts
+++ src/app/core/services/currencies.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { CurrencyResults } from '@interfaces/currency';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CurrenciesService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todas las monedas.
+   * @returns Observable de un array de monedas.
+   */
+  public getCurrencies(): Observable<CurrencyResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    let params = new HttpParams();
+    return this._http.get<CurrencyResults>(`${environment.apiUrl}currencies`, { headers, params });
+  }
 }
