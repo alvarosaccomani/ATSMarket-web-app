d7a660cd81871e8372c9be996dd57b101fed7e6c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-14 08:40:23 -0300

    add menu, menu-results and index interfaces to project

---
+++ src/app/core/interfaces/menu/menu.interface.ts
@@ -0,0 +1,13 @@
+export interface MenuInterface {
+    mnu_uuid: string | null,
+    mnu_parent_uuid: string | null,
+    mnu_title: string | null,
+    mnu_description: string | null,
+    mnu_icon: string | null,
+    mnu_route: string | null,
+    mnu_order: number | null,
+    mnu_itemactive: boolean | null,
+    mnu_active: boolean | null,
+    mnu_createdat: Date | null,
+    mnu_updatedat: Date | null
+}
