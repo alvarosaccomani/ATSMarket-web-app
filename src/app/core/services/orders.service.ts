849959b4a883e5da5ccd3fafff034dc8111ffdb9
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-15 19:55:32 -0300

    i add method getOrders to orders service

--- src/app/core/services/orders.service.ts
+++ src/app/core/services/orders.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { OrderResults } from '@interfaces/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene todas las ordenes de una empresa.
+   * En una aplicación real, aquí usarías this.http.get<IProduct[]>('URL_DE_TU_API').
+   * @returns Observable de un array de productos.
+   */
+  public getOrders(cmp_uuid: string): Observable<OrderResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+
+    let params = new HttpParams();
+
+    return this._http.get<OrderResults>(`${environment.apiUrl}orders/${cmp_uuid}`, { headers, params });
+  }
 }
