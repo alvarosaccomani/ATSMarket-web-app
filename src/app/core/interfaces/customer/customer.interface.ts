d5336b4a65a8b682a63f0797f400e83b3f994941
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-19 11:20:08 -0300

    add customer, customer-results and index interfaces to project

---
+++ src/app/core/interfaces/customer/customer.interface.ts
@@ -0,0 +1,10 @@
+export interface CustomerInterface {
+    usr_uuid: string;
+    cus_uuid: string;
+    cus_fullname: string;
+    cus_email: string;
+    cus_phone: string;
+    cus_dateofbirth: Date;
+    cus_createdat: Date;
+    cus_updatedat: Date;
+}
