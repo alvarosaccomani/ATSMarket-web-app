8637f62598e9773da10b6ce0e55b3a8e088d0250
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-19 11:20:46 -0300

    i add method getCustomerByUserId to customers service

--- src/app/core/services/customers.service.ts
+++ src/app/core/services/customers.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { CustomerResults } from '@interfaces/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Obtiene el cliente de un usuario.
+   * @param usr_uuid UUID del usuario.
+   * @returns Observable del cliente.
+   */
+  public getCustomerByUserId(usr_uuid: string): Observable<CustomerResults> {
+    const endpoint = `${environment.apiUrl}customer-by-user/${usr_uuid}`;
+    return this._http.get<CustomerResults>(endpoint);
+  }
 }
