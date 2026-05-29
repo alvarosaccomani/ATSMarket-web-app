ef2187bc50339fce5e51ab66eb158040ea6cd5e1
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-29 09:29:05 -0300

    i add super-admin component to project

---
+++ src/app/application/super-admin/super-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-super-admin',
+  imports: [],
+  templateUrl: './super-admin.component.html',
+  styleUrl: './super-admin.component.scss'
+})
+export class SuperAdminComponent {
+
+}
