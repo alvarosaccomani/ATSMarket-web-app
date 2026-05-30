0208f049db8aef2a07c3dc1e5983eac75a1dd47c
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-30 20:24:09 -0300

    i add method saveNotification to notifications service

--- src/app/core/services/notifications.service.ts
+++ src/app/core/services/notifications.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+
+  /**
+   * Crea una nueva notificacion.
+   */
+  public saveNotification(notification: any): Observable<{ data: any }> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    return this._http.post<{ data: any }>(`${environment.apiUrl}notification`, notification, { headers });
+  }
 }
