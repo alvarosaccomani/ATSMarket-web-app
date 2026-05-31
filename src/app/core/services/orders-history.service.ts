7a725f3764de962f3d3eef708aee475a3edb77af
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-05-31 19:33:47 -0300

    i add method getOrderHistory to orders-history service

--- src/app/core/services/orders-history.service.ts
+++ src/app/core/services/orders-history.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { OrderHistoryResults } from '@interfaces/order-history';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersHistoryService {
 
-  constructor() { }
+  constructor(
+    private _http: HttpClient
+  ) { }
+  
+  /**
+   * Obtiene todos los materiales.
+   * @returns Observable de un array con el historico de una orden.
+   */
+  public getOrderHistory(cmp_uuid: string, ord_uuid: string): Observable<OrderHistoryResults> {
+    const headers = new HttpHeaders().set('content-type', 'application/json');
+    let params = new HttpParams();
+    return this._http.get<OrderHistoryResults>(`${environment.apiUrl}order-history/${cmp_uuid}/${ord_uuid}`, { headers, params });
+  }
 }
