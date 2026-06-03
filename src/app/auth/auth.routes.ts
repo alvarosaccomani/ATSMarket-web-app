fba2d446055ca740dfc1a9d4b5d01eea234697a9
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-03 08:52:18 -0300

    i add unauthGuard to auth.routes.ts

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -2,13 +2,15 @@ import { Routes } from '@angular/router';
 
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { LoginComponent } from './login/login.component';
+import { unauthGuard } from '@guards/unauth.guard';
 
 export const AUTH_ROUTES: Routes = [
     {
         path: '',
         component: AuthLayoutComponent,
+        canActivate: [unauthGuard],
         children: [
             { path: 'login', component: LoginComponent }
         ]
     }
-];
+];
