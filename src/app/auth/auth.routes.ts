06a9d0d514f7a15bf2780333ea4a8ab9888c12ef
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-03 21:12:33 -0300

    i add register route to auth.routes.ts

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { unauthGuard } from '@guards/unauth.guard';
 
 export const AUTH_ROUTES: Routes = [
@@ -10,7 +11,8 @@ export const AUTH_ROUTES: Routes = [
         component: AuthLayoutComponent,
         canActivate: [unauthGuard],
         children: [
-            { path: 'login', component: LoginComponent }
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent }
         ]
     }
-];
+];
