d5c73f3059763e07ff4fba8f02a6e86b923d6afa
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-04 09:42:28 -0300

    i add account-confirmed/:usr_ConfirmationToken route to auth.routes.ts

--- src/app/auth/auth.routes.ts
+++ src/app/auth/auth.routes.ts
@@ -4,6 +4,7 @@ import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AccountCreatedComponent } from './account-created/account-created.component';
+import { AccountConfirmedComponent } from './account-confirmed/account-confirmed.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { unauthGuard } from '@guards/unauth.guard';
@@ -17,6 +18,7 @@ export const AUTH_ROUTES: Routes = [
             { path: 'login', component: LoginComponent },
             { path: 'register', component: RegisterComponent },
             { path: 'account-created/:usr_email', component: AccountCreatedComponent },
+            { path: 'account-confirmed/:usr_ConfirmationToken', component: AccountConfirmedComponent },
             { path: 'forgot-password', component: ForgotPasswordComponent },
             { path: 'reset-password/:usr_ResetPasswordToken', component: ResetPasswordComponent }
         ]
