2c71a93366dfdc6a688a2c85d26f77b3e728e42a
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-04 09:41:49 -0300

    i add account-confirmed component to project

---
+++ src/app/auth/account-confirmed/account-confirmed.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-confirmed',
+  imports: [],
+  templateUrl: './account-confirmed.component.html',
+  styleUrl: './account-confirmed.component.scss'
+})
+export class AccountConfirmedComponent {
+
+}
