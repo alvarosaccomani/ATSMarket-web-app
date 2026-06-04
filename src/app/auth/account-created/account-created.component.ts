4efce12dcdff2eb7783df666a40320232eb2cedf
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-06-04 09:36:15 -0300

    i add the usr_email parameter to the account-created component

--- src/app/auth/account-created/account-created.component.ts
+++ src/app/auth/account-created/account-created.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+
+// Imports de Ng-Zorro (necesarios para standalone)
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
 
 @Component({
   selector: 'app-account-created',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterLink,
+    NzButtonModule,
+    NzCardModule,
+    NzIconModule,
+    NzAlertModule
+  ],
   templateUrl: './account-created.component.html',
   styleUrl: './account-created.component.scss'
 })
-export class AccountCreatedComponent {
+export class AccountCreatedComponent implements OnInit {
+  usr_email: string = '';
 
+  constructor(private _route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.usr_email = this._route.snapshot.params['usr_email'] || '';
+  }
 }
