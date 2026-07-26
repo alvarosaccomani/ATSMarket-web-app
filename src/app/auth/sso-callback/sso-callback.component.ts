27b050e4da935dc0264e8bb3d0a1452a15fa5cbb
Alvaro Saccomani <alvaro.sacomani@gmail.com>
2026-07-26 12:03:03 -0300

    i add sso-callback component to project

---
+++ src/app/auth/sso-callback/sso-callback.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sso-callback',
+  imports: [],
+  templateUrl: './sso-callback.component.html',
+  styleUrl: './sso-callback.component.scss'
+})
+export class SsoCallbackComponent {
+
+}
